3793a6b24b308098eee4c61c0525ef2666533a63
sumantopal07 <sumantopal07@gmail.com>
2021-05-05 17:21:53 +0530

    cors

--- src/main/java/com/au/aums/controller/OppurtunityController.java
+++ src/main/java/com/au/aums/controller/OppurtunityController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.au.aums.dao.OppurtunityRepository;
@@ -21,6 +21,7 @@ import com.au.aums.model.dto.OppurtunityDTO;
 import com.au.aums.service.OppurtunityService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class OppurtunityController {
 	
 	
--- src/main/java/com/au/aums/controller/UserController.java
+++ src/main/java/com/au/aums/controller/UserController.java
@@ -1,25 +1,25 @@
 package com.au.aums.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.au.aums.model.Oppurtunities;
 import com.au.aums.model.User;
 import com.au.aums.model.dto.UserSignupDTO;
 import com.au.aums.service.UserService;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	
 	
